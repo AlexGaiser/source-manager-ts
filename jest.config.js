58be314d3270efbff24fc6d1b3096422243b0e17
alexgaiser <alexcgaiser@gmail.com>
2022-09-18 13:34:20 -0400

    exclude tests from compilation and exclude.d.ts files from testing

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
 module.exports = {
-  roots: ["<rootDir>"],
-  testMatch: [
-    "**/__tests__/**/*.+(ts|tsx|js)",
-    "**/?(*.)+(spec|test).+(ts|tsx|js)",
-  ],
+  roots: ['<rootDir>'],
+  testMatch: ['**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 };
