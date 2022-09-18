40d95d1227eba3d95115651ad50897b88140fc0b
alexgaiser <alexcgaiser@gmail.com>
2022-09-18 15:25:12 -0400

    0.0.36

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -3,7 +3,7 @@ RELEASE="patch" && [ -n "$1" ] && RELEASE=$1
 npm test
 npm version $RELEASE
 
-./build.sh -c
+npm run build
 
 git add src/config.ts
 
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const LIB_VERSION = "0.0.35";
+export const LIB_VERSION = "0.0.36";
