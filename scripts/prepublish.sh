2b04028e5141135f7193cf1238a421156df66775
alexgaiser <alexcgaiser@gmail.com>
2022-09-18 14:45:38 -0400

    update prepublish

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -2,3 +2,4 @@
 RELEASE="patch" && [ -n "$1" ] && RELEASE=$1
 npm test
 npm version $RELEASE
+npm run build:clean
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const LIB_VERSION = "0.0.29";
+export const LIB_VERSION = "0.0.30";
