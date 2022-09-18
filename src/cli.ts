c53b9d948e059f8c74b4620f07cfb20986b570db
alexgaiser <alexcgaiser@gmail.com>
2022-09-18 14:38:44 -0400

    add version flag

--- src/cli.ts
+++ src/cli.ts
@@ -1,42 +1,53 @@
 #! /usr/bin/env node
-import { getRootDir } from "./services/filemanager.service";
-import { Markdown } from "./services/Markdown";
+import { getRootDir } from './services/filemanager.service';
+import { Markdown } from './services/Markdown';
 import readline from 'readline';
-import { findSourceFile } from "./services/sources.service";
+import { findSourceFile } from './services/sources.service';
+import { LIB_VERSION } from './config';
+import { argv } from 'process';
 
 //https://stackoverflow.com/questions/18193953/waiting-for-user-to-enter-input-in-node-js
-function askQuestion(query):Promise<string> {
+function askQuestion(query): Promise<string> {
   const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
   });
 
-  return new Promise<string>(resolve => rl.question(`${query}\n`, ans => {
+  return new Promise<string>((resolve) =>
+    rl.question(`${query}\n`, (ans) => {
       rl.close();
       resolve(ans);
-  }))
+    }),
+  );
 }
 
-async function  main (){
-  const __rootDir = await getRootDir()
+async function main() {
+  const args = argv.slice(2);
+
+  if (args[0] === '-v' || args[0] === '--version') {
+    console.log(`v${LIB_VERSION}`);
+    process.exit();
+  }
+
+  const __rootDir = await getRootDir();
   const sourceFileName = findSourceFile(__rootDir);
-  if(!sourceFileName) {
-    console.log('Could not find Sources.md file')
-    process.exit()
+  if (!sourceFileName) {
+    console.log('Could not find Sources.md file');
+    process.exit();
   }
   const md = new Markdown(__rootDir, sourceFileName);
-  
-  console.log('The available link categories are:')
 
-  for(let header of md.subHeaders) {
-    console.log(header)
+  console.log('The available link categories are:');
+
+  for (let header of md.subHeaders) {
+    console.log(header);
   }
 
-  const linkSubHeading = await askQuestion('link subheading?')
-  const linkHREF =  await askQuestion('link href?')
-  const linkDescription = await askQuestion('link description?')
-  md.addLink(linkSubHeading, linkHREF, linkDescription)
+  const linkSubHeading = await askQuestion('link subheading?');
+  const linkHREF = await askQuestion('link href?');
+  const linkDescription = await askQuestion('link description?');
+  md.addLink(linkSubHeading, linkHREF, linkDescription);
   md.saveFile();
 }
 
-main()
+main();
