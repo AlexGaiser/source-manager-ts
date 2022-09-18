7e3043deb10bf0dbb8edecb7041d549fe6159987
alexgaiser <alexcgaiser@gmail.com>
2022-09-18 14:49:10 -0400

    update prepublish to update config

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -3,3 +3,6 @@ RELEASE="patch" && [ -n "$1" ] && RELEASE=$1
 npm test
 npm version $RELEASE
 npm run build:clean
+git add src/config.ts
+
+git commit --amend
--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const LIB_VERSION = "0.0.31";
+export const LIB_VERSION = "0.0.32";
